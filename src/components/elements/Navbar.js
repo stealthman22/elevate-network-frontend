0ad8c2336619981ddbf5bc453fe9a7fc8e2efb37
stealthman22 <tdotonline@gmail.com>
2021-01-06 14:31:45 +0000

    added the initial setup for navbar element

---
+++ src/components/elements/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="navbar bg-dark">
+    <h1>
+      <Link to="/">
+        {/* <i className="fas fa-code" />
+        {' '} */}
+        Elevate Network
+      </Link>
+    </h1>
+    <ul>
+      <li><Link to="/Events">Events</Link></li>
+      <li><Link to="/register">Register</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
