e67b47c3a5374bf16b2fe8041fd7e2a9349c202d
stealthman22 <tdotonline@gmail.com>
2021-02-05 19:36:38 +0000

    building out homepage with content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,68 @@
 import { React } from 'react';
-import HeroSlider from './elements/HeroSlider';
+import Hero from './elements/Hero';
 
 const Home = () => (
   <>
     <div className="home-ctn">
 
-      <HeroSlider />
+      <Hero />
+      <div className="values-ctn">
+        <h1 className="large text-center text-primary">Values</h1>
+
+        <div className="value-card">
+          <div className="value-card-img vci-1" />
+          <div className="value-card-content">
+            <span>
+            We empower students to tap
+             into their
+              {' '}
+              <br />
+              {' '}
+               full potential
+            </span>
+          </div>
+        </div>
+
+        <div className="value-card">
+          <div className="value-card-img vci-1" />
+          <div className="value-card-content">
+            <span>
+            We co-create long-term positive
+impact in collaboration with all our
+stakeholders
+              {' '}
+              <br />
+              {' '}
+               full potential
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="highlight-ctn">
+        <div className="highlight-card">
+          {' '}
+          <p>
+Elevate Network Community is an educational
+platform where students can learn STEM, Art &
+Design, Entrepreneurship, etc. Made up of highly
+talented creatives, innovators and entrepreneurs.
+We are all driven by a single goal: creating content
+that will help you succeed.
+          </p>
+
+        </div>
+        <div className="highlight-card">
+          <p>
+            {' '}
+Made up of highly
+talented creatives, innovators and entrepreneurs.
+We are all driven by a single goal: creating content
+that will help you succeed.
+          </p>
+        </div>
+        <div className="highlight-card">Lorem ipsum dolor sit amet.</div>
+      </div>
     </div>
   </>
 );
---
+++ src/components/elements/Hero.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+
+const Hero = () => (
+
+  <div className="hero-ctn">
+    <div className="overlay">
+      <div className="headline">
+        <h1>Elevate Network</h1>
+        <p>Inspire, Empower, Achieve</p>
+      </div>
+      <div className="content">
+        <div className="content-card content-card1">
+          <span>
+          The Elevate Network Community is
+           a unique and premier program designed to help students level up academically, personally
+           and professionally...
+          </span>
+        </div>
+
+      </div>
+    </div>
+  </div>
+);
+
+export default Hero;
