e31bd3e9e3431fdb05c0c8ee6e9265d35f67a251
stealthman22 <tdotonline@gmail.com>
2021-02-09 21:00:32 +0000

    done with homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import { React } from 'react';
 import { Link } from 'react-router-dom';
 import Hero from './elements/Hero';
 import CallToAction from './elements/CallToAction';
+import Footer from './elements/Footer';
 
 const Home = () => (
   <>
@@ -175,21 +176,24 @@ const Home = () => (
             </div>
           </div>
         </div>
-        <div className="modules-ctn">
-          <h2 className="large text-center text-primary">Our Modules</h2>
-          <div className="modules-wrapper">
-            <div className="module-card">
-              <h4 className="text-center lead"><i className="fas fa-robot"> STEM</i></h4>
+      </div>
 
-              <p className="text-chunk">
+      <div className="modules-ctn">
+        <h2 className="large text-center text-primary">Our Modules</h2>
+
+        <div className="modules-wrapper">
+          <div className="module-card">
+            <h4 className="text-center lead"><i className="fas fa-robot"> STEM</i></h4>
+
+            <p className="text-chunk">
             Internationally, there is a strong gender imbalance in
             the STEM fields of science, technology, engineering and
             mathematics. The STEM project aims to help bridge
             that gap through access to education, mentorship
             opportunities and leadership training.
-              </p>
+            </p>
 
-              <p>
+            <p>
                 Our STEM program
                 is a lifetime opportunity for school students to dive into
                 the field of STEM (Science, Technology, Engineering,
@@ -197,204 +201,154 @@ const Home = () => (
                 develop their leadership skills, in a unique cross-cultural
                 environment. The STEM project will focus on Robotics, AI,
                 and Programming.
-              </p>
-            </div>
+            </p>
+          </div>
 
-            <div className="module-card">
+          <div className="module-card">
 
-              <h4 className="text-center lead">
-                {' '}
-                <i className="fa fa-paint-brush" aria-hidden="true">ART AND DESIGN</i>
-              </h4>
-              <p className="text-chunk">
-                {' '}
+            <h4 className="text-center lead">
+              {' '}
+              <i className="fa fa-paint-brush" aria-hidden="true">ART AND DESIGN</i>
+            </h4>
+            <p className="text-chunk">
+              {' '}
                 Art is a profound act of expression, and when it meets
                 design, it results in meaningful concepts and objects that
                 shape our day-to-day lives.
-                Elevate Network programs are designed to equip school students with a comprehensive
-toolbox, combining traditional skills like drawing and
-sculpting with digital techniques producing high-end
-design work.
-              </p>
+                              Elevate Network programs are designed to equip school students with a comprehensive
+              toolbox, combining traditional skills like drawing and
+              sculpting with digital techniques producing high-end
+              design work.
+            </p>
 
-              <p>
-This program combines art fundamentals
-with the design industry’s current technology – helping
-students expand their artistic talents to create powerful
-imagery for entertainment, advertising, branding, and
-the web.
-              </p>
-            </div>
+            <p>
+              This program combines art fundamentals
+              with the design industry’s current technology – helping
+              students expand their artistic talents to create powerful
+              imagery for entertainment, advertising, branding, and
+            the web.
+            </p>
+          </div>
 
-            <div className="module-card">
-              <h4 className="text-center lead">
-                {' '}
-                <i className="fas fa-briefcase"> ENTREPRENEURSHIP</i>
-              </h4>
-              <p className="text-chunk">
-                {' '}
+          <div className="module-card">
+            <h4 className="text-center lead">
+              {' '}
+              <i className="fas fa-briefcase"> ENTREPRENEURSHIP</i>
+            </h4>
+            <p className="text-chunk">
+              {' '}
               We believe Entrepreneurship is the key to Africa’s
               continuous development. To fulfill this vision for our
               nation and continent, we train students to turn their
               innovative and creative ideas into impactful brands.
-              </p>
+            </p>
 
-              <p>
+            <p>
               Based on the type of brand the student is interested in
               starting, we will connect them with an Entrepreneur who
               is already established in that field to teach and provide
               resources. We believe the best way to improve and learn
               is by shadowing someone who is already doing what you
               want to do.
-              </p>
-            </div>
+            </p>
           </div>
         </div>
+      </div>
+      <CallToAction />
+      <div className="portfolio-ctn">
+        <h2 className="large text-center text-primary">Our Portfolio</h2>
+        <div className="portfolio-wrapper">
+          <div className="community">
+            <h4 className="  text-center ">COMMUNITY SUPPORT</h4>
+            <div className="comm-1">
+              <div className="port-card">
+                <i className="fas fa-lightbulb"> IDEA LAB</i>
+                <p>
+                  Supporting potential students
+                   to develop their ideas into
+                actual business.
+                </p>
+              </div>
 
-        <div className="portfolio-ctn">
-          <h2 className="large text-center text-primary">Our Portfolio</h2>
-          <div className="portfolio-wrapper">
-            <div className="community">
-              <h4 className="  text-center ">COMMUNITY SUPPORT</h4>
-              <div className="comm-1">
-                <div className="port-card">
-                  <i className="fas fa-lightbulb"> IDEA LAB</i>
-                  <p>
-          Supporting potential students
-          to develop their ideas into
-          actual business.
-                  </p>
-                </div>
-
-                <div className="port-card">
-                  <i className="fa fa-users" aria-hidden="true"> MENTORING PROGRAMS</i>
-                  <p>
+              <div className="port-card">
+                <i className="fa fa-users" aria-hidden="true"> MENTORING PROGRAMS</i>
+                <p>
                 Matching community members with skillful and seasoned
                 entrepreneurs from various backgrounds.
-                  </p>
-                </div>
-
-                <div className="port-card">
-                  <i className="fas fa-egg"> INCUBATION</i>
-                  <p>
-                Building students ideas via connections with corporates,
-networks and tailor-made tools
-                    {' '}
-                    <br />
-                    {' '}
-and providing students
-with the resources they need to make a difference.
-                  </p>
-                </div>
+                </p>
               </div>
 
-              <div className="comm-2">
-                <div className="port-card">
-                  <i className="fas fa-box"> LAUNCH BOX</i>
-                  <p>
-Collection of training materials from the whole Elevate
-product portfolio
-                  </p>
-                </div>
-
-                <div className="port-card">
-                  <i className="fas fa-home"> ELEVATE HOUSE</i>
-                  <p>
-                A unique combination of workspace, networking hub, and
-learning environment
-                  </p>
-                </div>
-
-                <div className="port-card">
-                  <i className="fas fa-shuttle-van"> EXPEDITION</i>
-                  <p>
-              Vehicle for students and ecosystem actors interested in
-              learning from each other and accessing new markets
-                  </p>
-                </div>
+              <div className="port-card">
+                <i className="fas fa-egg"> INCUBATION</i>
+                <p>
+                Building students ideas via connections with corporates,
+            networks and tailor-made tools
+                  {' '}
+                  <br />
+                  {' '}
+                and providing students
+                  with the resources they need to make a difference.
+                </p>
               </div>
             </div>
-            <div className="organization">
-              <h4 className="  text-center ">ORGANIZATIONAL SUPPORT</h4>
+
+            <div className="comm-2">
               <div className="port-card">
-                <i className="fas fa-user-tie"> CHAPTER MANAGER MENTOR (CMM)</i>
+                <i className="fas fa-box"> LAUNCH BOX</i>
                 <p>
+                Collection of training materials from the whole Elevate
+                product portfolio
+                </p>
+              </div>
+
+              <div className="port-card">
+                <i className="fas fa-home"> ELEVATE HOUSE</i>
+                <p>
+                A unique combination of workspace, networking hub, and
+                learning environment
+                </p>
+              </div>
+
+              <div className="port-card">
+                <i className="fas fa-shuttle-van"> EXPEDITION</i>
+                <p>
+              Vehicle for students and ecosystem actors interested in
+              learning from each other and accessing new markets
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="organization">
+            <h4 className="  text-center ">ORGANIZATIONAL SUPPORT</h4>
+            <div className="port-card">
+              <i className="fas fa-user-tie"> CHAPTER MANAGER MENTOR (CMM)</i>
+              <p>
               A tailor-made curriculum promoting the mindset of
               mentoring & empowering chapter managers and
               executives.
-                </p>
-              </div>
+              </p>
+            </div>
 
-              <div className="port-card">
-                <i className="fas fa-comments"> PARENT - TEACHER SEGMENT</i>
-                <p>
+            <div className="port-card">
+              <i className="fas fa-comments"> PARENT - TEACHER SEGMENT</i>
+              <p>
               Intensive training & solution-oriented curriculum for
               parents and teachers from different sectors.
-                </p>
-              </div>
+              </p>
+            </div>
 
-              <div className="port-card">
-                <i className="fas fa-handshake"> CONSULTING & ADVISORY</i>
-                <p>
+            <div className="port-card">
+              <i className="fas fa-handshake"> CONSULTING & ADVISORY</i>
+              <p>
               Tailor-made services for students, individuals and brands.
-                </p>
-              </div>
+              </p>
             </div>
           </div>
         </div>
       </div>
 
-      {/*
+      <Footer />
 
-          <p className="text-chunk">
-Life brings challenges and those challenges must be
-met with a plan to win against all odds.
-Many successful people in the world have mentors.
-          </p>
-
-          <p className="text-chunk">
-Mentors pave the way for mentees to continue to walk
-in their chosen path. They provide personal experiences,
-guidance, motivation, role modeling and more
-importantly emotional support to their respective students beyond the household or school.
-          </p>
-          <p className="text-chunk">
-They push students to explore different
-career or entrepreneurial options,
-developing new networks or contacts, identifying
-many resources and setting big goals.
-          </p>
-          <p>
-          Students are known to stay within their comfort zones and a mentor
-is designed to keep them striving beyond their comfort
-zones and exploring new options through, STEM,
-Entrepreneurship, Art/Design, and developing stronger emotional intelligence.
-          </p>
-
-      <div className="highlight-ctn">
-        <div className="highlight-card">
-          {' '}
-          <p>
-Elevate Network Community is an educational
-platform where students can learn STEM, Art &
-Design, Entrepreneurship, etc. Made up of highly
-talented creatives, innovators and entrepreneurs.
-We are all driven by a single goal: creating content
-that will help you succeed.
-          </p>
-
-        </div>
-        <div className="highlight-card">
-          <p>
-            {' '}
-Made up of highly
-talented creatives, innovators and entrepreneurs.
-We are all driven by a single goal: creating content
-that will help you succeed.
-          </p>
-        </div>
-        <div className="highlight-card">Lorem ipsum dolor sit amet.</div>
-      </div> */}
     </div>
   </>
 );
---
+++ src/components/elements/Footer.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => (
+  <footer className="footer-ctn">
+    <div className="footer-card">
+      <div className="footer-content">
+        <div className="cta text-center">
+         Never miss out on what Elevate is doing:
+
+          {' '}
+          <Link to="/register"><span className="btn btn-primary"> Register Now</span></Link>
+
+        </div>
+        <div className="smb">
+          <span className="ml">Follow us on social media:</span>
+          <br />
+          <div className="smb-icons">
+
+            <a href="https://twitter.com/elevatenetwork1" target="_blank" rel="noreferrer" className="smb-icon">
+              <i className="fab fa-twitter-square fa-3x " />
+              <p> Twitter</p>
+            </a>
+
+            <a href="https://instagram.com/elevatenetworkhq/" target="_blank" rel="noreferrer" className="smb-icon">
+              {' '}
+              <i className="fab fa-instagram-square fa-3x " />
+              <p>Instagram</p>
+            </a>
+
+            {' '}
+            <a href="https://linkedin.com/company/elevatenetwork/" target="_blank" rel="noreferrer" className="smb-icon">
+              {' '}
+              <i className="fab fa-linkedin fa-3x " />
+              <p>Linkedin</p>
+            </a>
+
+          </div>
+        </div>
+        <div className="contact">
+          <span className="ml">
+For enquiries mail or call us @:
+          </span>
+
+          <br />
+          <div className="mail-icons">
+            <a href="mailto:info@elevatenetworkhq.com" target="_blank" rel="noreferrer" className="mail-icon">
+              <i className="fas fa-paper-plane fa-2x" />
+              <p>Info</p>
+            </a>
+
+            <a href="mailto:partnerships@elevatenetworkhq.com" target="_blank" rel="noreferrer" className="mail-icon">
+              <i className="fas fa-envelope-open-text fa-2x" />
+              <p>Partnerships</p>
+            </a>
+
+            <a href="mailto:admin@elevatenetworkhq.com" target="_blank" rel="noreferrer" className="mail-icon">
+              {' '}
+              <i className="fas fa-envelope-open-text fa-2x" />
+              {' '}
+              <p> Admin</p>
+            </a>
+            <a href="tel: +233508800813" target="_blank" rel="noreferrer" className="mail-icon">
+              {' '}
+              <i className="fas fa-phone fa-2x" />
+              {' '}
+              <p> Call</p>
+            </a>
+          </div>
+        </div>
+        <p>
+          {' '}
+          <i className="far fa-copyright copy"> Elevate Network 2021</i>
+        </p>
+      </div>
+
+    </div>
+
+  </footer>
+);
+
+export default Footer;
